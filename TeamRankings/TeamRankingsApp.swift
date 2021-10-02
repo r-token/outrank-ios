a576abc5e38ae2b7e1c984454c00d3c3d77dbb29
Ryan Token <ryantoken13@gmail.com>
2021-10-02 14:55:08 -0500

    Initial Commit

---
+++ TeamRankings/TeamRankingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamRankingsApp.swift
+//  TeamRankings
+//
+//  Created by Ryan Token on 10/2/21.
+//
+
+import SwiftUI
+
+@main
+struct TeamRankingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
